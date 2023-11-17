75925075cf55c9dc61861b65e20a1a6988e22020
FarryDay <Mokrushin29@yandex.ru>
2023-11-17 23:56:18 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,27 @@
-import styled from '@emotion/styled'
 import CloudRoundedIcon from '@mui/icons-material/CloudRounded'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Main.module.scss'
 
-const Clock = () => {
-	const clock = new Date()
-	const time = clock.getHours() + ':' + clock.getMinutes()
-	return <h1>{time}</h1>
-}
-
-const WeatheIcon = styled(CloudRoundedIcon)({})
-
 export default function Main() {
+	const [time, setTime] = useState('00:00')
+
+	useEffect(() => {
+		setInterval(() => {
+			const date = new Date()
+			setTime(`${date.getHours()}:${date.getMinutes()}`)
+		}, 5000)
+	}, [])
+
 	return (
 		<>
 			<div className={styles.weather}>
 				<div className={styles.generaly}>
 					<div className={styles.tempature}></div>
 					<div className={styles.weatherdisplay}>
-						<WeatheIcon sx={{ color: 'white', fontSize: '15rem' }}></WeatheIcon>
-						<Clock />
+						<CloudRoundedIcon
+							sx={{ color: 'white', fontSize: '15rem' }}
+						></CloudRoundedIcon>
+						<h1>{time}</h1>
 					</div>
 				</div>
 				<div className={styles.secondary}></div>
