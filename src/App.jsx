4f8b454aa4a5ae8e5f0e3ac5a7f560380c78eab4
FarryDay <Mokrushin29@yandex.ru>
2023-11-17 22:35:30 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.module.scss'
+import styles from './App.module.scss'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 
 function App() {
 	return (
-		<main className='main'>
+		<main className={styles.main}>
 			<Header />
 			<Main />
 			<Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Search } from '@mui/icons-material'
+import FormControl from '@mui/material/FormControl'
+import Input from '@mui/material/Input'
+import InputAdornment from '@mui/material/InputAdornment'
+import React, { useState } from 'react'
 
 export default function Header() {
+	const [value, setValue] = useState('')
 	return (
 		<div>
-			<h1>Hello World!</h1>
+			<FormControl variant='standard'>
+				<Input
+					id='search'
+					placeholder='Change city'
+					value={value}
+					onChange={(e, value) => setValue(() => value)}
+					startAdornment={
+						<InputAdornment position='start'>
+							<Search />
+						</InputAdornment>
+					}
+				/>
+			</FormControl>
 		</div>
 	)
 }
