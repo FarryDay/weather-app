dd0efc824021627ec1e56ea391a9fb29e296d3e7
FarryDay <Mokrushin29@yandex.ru>
2023-11-17 23:44:47 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,13 @@
+import styled from '@emotion/styled'
+import CloudRoundedIcon from '@mui/icons-material/CloudRounded'
 import React from 'react'
 import styles from './Main.module.scss'
-import CloudRoundedIcon from '@mui/icons-material/CloudRounded'
-import styled from '@emotion/styled'
-import { colors } from '@mui/material'
-const Clock = () =>{
+const Clock = () => {
 	const clock = new Date()
 	const time = clock.getHours() + ':' + clock.getMinutes()
 	return <h1>{time}</h1>
 }
-const WeatheIcon = styled(CloudRoundedIcon)({
-	
-})
+const WeatheIcon = styled(CloudRoundedIcon)({})
 export default function Main() {
 	return (
 		<>
@@ -21,8 +18,8 @@ export default function Main() {
 						<WeatheIcon sx={{ color: 'white', fontSize: '15rem' }}></WeatheIcon>
 						<Clock />
 					</div>
-					<div className={styles.secondary}></div>
 				</div>
+				<div className={styles.secondary}></div>
 			</div>
 		</>
 	)
