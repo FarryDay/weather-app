533034630e7c1dc6b14c0a1e52c32254437e0202
alaska <rus.rakhmatullin.06@bk.ru>
2023-11-18 14:06:17 +0300

    Weather app

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,12 +5,17 @@ import styles from './Main.module.scss'
 
 export default function Main() {
 	const [time, setTime] = useState('00:00')
+	const [date, setDate] = useState('')
+	const [weatherData, setWeatherData] = useState({})
 
+	window.store.subscribe(() => setWeatherData(window.store.getState()))
+	
 	useEffect(() => {
-		setTime(`${moment().format('hh:mm')}`)
-
+		setTime(`${moment().format('hh:mm a')}`)
+		setDate(`${moment().format(' - YY.MM.DD')}`)
 		setInterval(() => {
-			setTime(`${moment().format('hh:mm')}`)
+			setDate(`${moment().format(' - YY.MM.DD')}`)
+			setTime(`${moment().format('hh:mm a')}`)
 		}, 5000)
 	}, [])
 
@@ -18,7 +23,11 @@ export default function Main() {
 		<>
 			<div className={styles.weather}>
 				<div className={styles.generaly}>
-					<div className={styles.tempature}>11 °C</div>
+					<div className={styles.tempature}>
+						<h3>{weatherData.temp}</h3>
+						<h4>{weatherData.location}</h4>
+						<p className={styles.date}>{date}</p>
+					</div>
 					<div className={styles.weatherdisplay}>
 						<CloudRoundedIcon
 							sx={{ color: 'white', fontSize: '15rem' }}></CloudRoundedIcon>
@@ -27,9 +36,9 @@ export default function Main() {
 				</div>
 				<div className={styles.secondary}>
 					<div className={styles.secondaryindicators}>
-						<p>Влажность:</p>
-						<p>Давление:</p>
-						<p>Ветер:</p>
+						<h3>Влажность:<p>{weatherData.humidity}</p></h3>
+						<h3>Давление:<p>{weatherData.pressure}</p></h3>
+						<h3>Ветер:<p>{weatherData.wind}</p></h3>
 					</div>
 					<div className={styles.secondaryinfo}>
 						<h1>info</h1>
