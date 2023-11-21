a7e49b656542f988941808c21a83ccd43268e6cc
FarryDay <Mokrushin29@yandex.ru>
2023-11-21 08:34:24 +0300

    Change cards

--- src/components/Footer/Card.jsx
+++ src/components/Footer/Card.jsx
@@ -1,11 +1,22 @@
+import moment from 'moment'
+import 'moment/locale/ru'
 import React from 'react'
+import getDayOfWeek from '../../utils/getDayOfWeek'
 import styles from './Footer.module.scss'
 
 export default function Card({ data }) {
+	console.log(data)
+	const date = new Date(data.date)
 	return (
 		<div className={styles.card}>
 			<img src={data.day.condition.icon} alt={data.day.condition.text} />
-			<h2 style={{ color: 'white' }}>{data.day.avgtemp_c}</h2>
+			<p style={{ color: 'white', fontWeight: 700 }}>
+				{moment(date).format('DD.MM')}
+			</p>
+			<h1 style={{ color: 'white', fontSize: 24, fontWeight: 100 }}>
+				{getDayOfWeek(date.getDay())}
+			</h1>
+			<h2 style={{ color: 'white' }}>{data.day.avgtemp_c} °С</h2>
 		</div>
 	)
 }
